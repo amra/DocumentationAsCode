263f42f8c66a1c7975ff43ad005401daeb3196ff
Marek Hudik <hudik.marek@gmail.com>
2018-05-12 16:40:52 +0200

    wkhtmltopdf: header and footer test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # wkhtmltopdf
-docker run -w /foo -v `pwd`/test:/foo amra/documentation-as-code pandoc --standalone --number-sections --pdf-engine=wkhtmltopdf wkhtmltopdf.md -o wkhtmltopdf.pdf
+docker run -w /foo -v `pwd`/test:/foo amra/documentation-as-code pandoc --standalone --number-sections --table-of-contents --pdf-engine=wkhtmltopdf wkhtmltopdf.md -o wkhtmltopdf.pdf
