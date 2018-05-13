0ac0a55319f017abfad5cb1e98386dd1f6f117cf
Marek Hudik <hudik.marek@gmail.com>
2018-05-13 21:19:56 +0200

    intro page

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # wkhtmltopdf
-docker run -w /foo -v `pwd`/test:/foo -v `pwd`/template:/foo/template amra/documentation-as-code pandoc --self-contained --standalone --number-sections --table-of-contents --pdf-engine=wkhtmltopdf wkhtmltopdf.md -o wkhtmltopdf.pdf --template template/pandoc-bootstrap-template.html
+docker run -w /foo -v `pwd`/test:/foo -v `pwd`/template:/foo/template amra/documentation-as-code pandoc --self-contained --standalone --number-sections --table-of-contents --pdf-engine=wkhtmltopdf wkhtmltopdf.md -o wkhtmltopdf.pdf --template template/pandoc-bootstrap-template.html --include-before-body wkhtmltopdf-intro.html
