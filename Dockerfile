6285d0dd2cc8d0c7a226d40a9e499e42cdb0514f
Marek Hudik <hudik.marek@gmail.com>
2018-05-17 23:54:52 +0200

    weasyprint and test files

--- Dockerfile
+++ Dockerfile
@@ -23,3 +23,8 @@ ADD https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmlto
 RUN cd /tmp && tar -xf /tmp/wkhtmltox.tar.xz wkhtmltox/bin/wkhtmltopdf
 RUN mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
 RUN wkhtmltopdf -V
+
+# WeasyPrint
+RUN apt-get install -y build-essential python3-dev python3-pip python3-setuptools \
+    python3-wheel python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
+RUN pip3 install WeasyPrint
--- test.sh
+++ test.sh
@@ -2,3 +2,6 @@
 
 # wkhtmltopdf
 docker run -w /foo -v `pwd`/test:/foo -v `pwd`/template:/foo/template amra/documentation-as-code pandoc --self-contained --standalone --number-sections --table-of-contents --pdf-engine=wkhtmltopdf wkhtmltopdf.md -o wkhtmltopdf.pdf --template template/pandoc-bootstrap-template.html --include-before-body wkhtmltopdf-intro.html
+
+# WeasyPrint
+docker run -w /foo -v `pwd`/test:/foo -v `pwd`/template:/foo/template amra/documentation-as-code pandoc --self-contained --standalone --number-sections --table-of-contents --pdf-engine=weasyprint weasyprint.md -o weasyprint.pdf --template template/pandoc-bootstrap-template.html --include-before-body weasyprint-intro.html
