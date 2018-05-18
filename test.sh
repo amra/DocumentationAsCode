fe7a727566e907fb880b2a75f7d6762fbf4b1714
Marek Hudík <marek.hudik@deutsche-boerse.com>
2018-05-18 17:53:49 +0200

    test directory mapping

--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 mkdir -p output
 
+BASE=`pwd`
+
 # wkhtmltopdf
 echo "wkhtmltopdf *************************************************************"
-docker run -w /foo -v `pwd`/test/output:/foo/output -v `pwd`/test/wkhtmltopdf:/foo -v `pwd`/test/image:/foo/image -v `pwd`/template:/foo/template amra/documentation-as-code \
-pandoc --self-contained --standalone --number-sections --table-of-contents --pdf-engine=wkhtmltopdf wkhtmltopdf.md -o output/wkhtmltopdf.pdf --template template/pandoc-bootstrap-template.html --include-before-body wkhtmltopdf-intro.html
+docker run -w /foo -v $BASE/test/output:/foo/output -v $BASE/test/wkhtmltopdf:/foo/input -v $BASE/test/image:/foo/image -v $BASE/template:/foo/template amra/documentation-as-code \
+pandoc --self-contained --standalone --number-sections --table-of-contents --pdf-engine=wkhtmltopdf input/wkhtmltopdf.md -o output/wkhtmltopdf.pdf --template template/pandoc-bootstrap-template.html --include-before-body input/wkhtmltopdf-intro.html
 
 # WeasyPrint
 echo -e "\n\n\n"
 echo "weasyprint **************************************************************"
-docker run -w /foo -v `pwd`/test/output:/foo/output -v `pwd`/test/weasyprint:/foo -v `pwd`/test/image:/foo/image -v `pwd`/template:/foo/template amra/documentation-as-code \
-pandoc --self-contained --standalone --number-sections --table-of-contents --pdf-engine=weasyprint weasyprint.md -o output/weasyprint.pdf --template template/pandoc-bootstrap-template.html --include-before-body weasyprint-intro.html
+docker run -w /foo -v $BASE/test/output:/foo/output -v $BASE/test/weasyprint:/foo/input -v $BASE/test/image:/foo/image -v $BASE/template:/foo/template amra/documentation-as-code \
+pandoc --self-contained --standalone --number-sections --table-of-contents --pdf-engine=weasyprint input/weasyprint.md -o output/weasyprint.pdf --template template/pandoc-bootstrap-template.html --include-before-body input/weasyprint-intro.html
