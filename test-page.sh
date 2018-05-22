b7411715898425d7c70dc5cf787371e5b735914a
Marek Hudík <marek.hudik@deutsche-boerse.com>
2018-05-22 17:51:49 +0200

    test: print orientation

---
+++ test-page.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir -p output
+mkdir -p /tmp/work
+rm /tmp/work/* -rf
+
+BASE=`pwd`
+
+# html
+echo "html *************************************************************"
+
+docker run -w /foo \
+   -v /tmp/work:/foo \
+   -v $BASE/output:/foo/output \
+   -v $BASE/test/html:/foo/input \
+   -v $BASE/test/image:/foo/image \
+   -v $BASE/template/pandoc-goodies/templates/html5/github/GitHub.html5:/root/.pandoc/templates/template.html \
+   amra/documentation-as-code \
+pandoc --self-contained --standalone --number-sections --table-of-contents input/html-landscape.md -o output/html_pandoc-goodies-print.html --template template --include-before-body input/html-intro-page-break.html
